55610345d6d1a32b8eb2430963cbce71046dacac
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2025-09-02 01:33:51 +0530

    update cors configuration setup for production

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,13 @@ const app = express()
 
 app.use(express.json())
 
-const allowedOrigins = [
-  process.env.FRONTEND_URL1,
-  process.env.FRONTEND_URL2
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); 
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+app.use(cors({
+  origin: [process.env.FRONTEND_URL1, process.env.FRONTEND_URL2], 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
   credentials: true, 
-};
+}));
 
-app.use(cors(corsOptions));
 app.use("/images/storage", express.static("uploads"))
 
 app.use("/api/v1.0", routes);
